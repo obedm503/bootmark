aa273e866fa910a1dcb3219d1d52e8021a871c80
obedm503 <obedm503@gmail.com>
2017-03-25 00:37:02 -0600

    finished adjusting kitchen sink

--- src/bootmark.js
+++ src/bootmark.js
@@ -202,11 +202,11 @@
       */
       unescape: function unescape(text){
         return text
-          .replace(/&amp;/g, '&')
           .replace(/&lt;/g, '<')
           .replace(/&gt;/g, '>')
           .replace(/&quot/g, '"')
-          .replace(/&#39/g, '\'');
+          .replace(/&#39/g, '\'')
+          .replace(/&amp;/g, '&');
       },
 
       /**
