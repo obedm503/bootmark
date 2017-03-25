6b19fb3d365ddadfbf70201b28c97c78e5fefb3c
obedm503 <obedm503@gmail.com>
2017-03-25 11:49:05 -0600

    fixed https problem in

--- src/bootmark.js
+++ src/bootmark.js
@@ -3,7 +3,6 @@
 * @author [obedm503](https://github.com/obedm503/)
 * @git [git repo](https://github.com/obedm503/bootmark.git)
 * @examples [examples/starters/templates](https://obedm503.github.io/bootmark/docs/examples.html)
-* @version 0.8.0
 * @license MIT
 */
 (function($, document){
